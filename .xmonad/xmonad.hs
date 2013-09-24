bc7dd428ce2d3841fea70c74ce35a661c7706ca1
Ted Cooper <anthezium@gmail.com>
2013-09-23 21:20:00 -0700

    Switched modMask to Windows Key Set up for easy change to managing workspaces for Gnome

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,5 +1,6 @@
 import XMonad
 import XMonad.Actions.CycleWS
+import XMonad.Config.Gnome
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Layout.IM
@@ -11,23 +12,25 @@ import System.IO
 
 gimp = withIM (0.11) (Role "gimp-toolbox") $ reflectHoriz $ withIM (0.15) (Role "gimp-dock") Full
 
+myModMask = mod4Mask
+
 main = do
   xmobarProc <- spawnPipe "/home/ted/bin/start_xmobar.sh /home/ted/.xmobarrc"
-  xmonad $ defaultConfig 
+  xmonad $ defaultConfig --gnomeConfig to manage windows for Gnome
     { manageHook = manageDocks <+> manageHook defaultConfig
     , layoutHook = avoidStruts  $  (onWorkspace "gimp" gimp) $ layoutHook defaultConfig
     , logHook = dynamicLogWithPP xmobarPP
         { ppOutput = hPutStrLn xmobarProc
         , ppTitle = xmobarColor "green" "" . shorten 50
         }
-    , modMask = mod1Mask
+    , modMask = myModMask
     , terminal = "xterm"
     } `additionalKeys` 
-    [ ((mod1Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
+    [ ((myModMask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
     , ((0, xK_Print), spawn "scrot '%Y-%m-%d_$wx$h_full.png' -e 'mv $f ~/screenshots/'") --screenshot
     , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s '%Y-%m-%d_$wx$h_window.png' -e 'mv $f ~/shots/'") --windowshot
-    , ((mod1Mask, xK_Left), prevWS)
-    , ((mod1Mask, xK_Right), nextWS)
-    , ((mod1Mask .|. shiftMask , xK_Left), shiftToPrev)
-    , ((mod1Mask .|. shiftMask , xK_Right), shiftToNext)
+    , ((myModMask, xK_Left), prevWS)
+    , ((myModMask, xK_Right), nextWS)
+    , ((myModMask .|. shiftMask , xK_Left), shiftToPrev)
+    , ((myModMask .|. shiftMask , xK_Right), shiftToNext)
     ]
