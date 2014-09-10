69d86102c4fb35089ae91c9bba7d5f1e4e304726
Ted Cooper <anthezium@gmail.com>
2014-09-10 07:16:55 -0700

    set up xbindkeys so mute, volume, and mic mute buttons work

---
+++ .xbindkeysrc.scm
@@ -0,0 +1,4 @@
+(xbindkey '("XF86AudioRaiseVolume") "amixer -c 0 sset 'Master',0 2dB+")
+(xbindkey '("XF86AudioLowerVolume") "amixer -c 0 sset 'Master',0 2dB-")
+(xbindkey '("XF86AudioMute") "amixer set Master toggle")
+(xbindkey '("XF86AudioMicMute") "amixer set Capture toggle") 
