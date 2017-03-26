33c416c14d0d22c027da9a8105cd7c2fde4da6d8
Ted Cooper <anthezium@gmail.com>
2017-03-25 20:53:16 -0700

    fixd

--- bin/youtube_backup.sh
+++ bin/youtube_backup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-pushd /mnt/KingGhidora/media/shared/youtube_likes && \
-youtube-dl --no-progress -i --download-archive archive_list.txt --audio-quality 0 https://www.youtube.com/playlist?list=LLBkKM6OCxNzgW8TWxZeDn_w 2 >&1 >> log && \
+set -e
+
+pushd /mnt/KingGhidora/media/shared/youtube_likes 
+date >> log
+/usr/local/bin/youtube-dl --no-progress -i --download-archive archive_list.txt --audio-quality 0 https://www.youtube.com/playlist?list=LLBkKM6OCxNzgW8TWxZeDn_w 2 >&1 >> log 
 popd
