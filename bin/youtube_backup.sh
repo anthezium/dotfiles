9c1d3f684f490e6072b272ff2b7b615e79501422
Ted Cooper <anthezium@gmail.com>
2017-03-27 00:18:29 -0700

    added soundcloud likes

--- bin/youtube_backup.sh
+++ bin/youtube_backup.sh
@@ -6,3 +6,8 @@ pushd /mnt/KingGhidora/media/shared/youtube_likes
 date >> log
 /usr/local/bin/youtube-dl --no-progress -i --download-archive archive_list.txt --audio-quality 0 https://www.youtube.com/playlist?list=LLBkKM6OCxNzgW8TWxZeDn_w 2 >&1 >> log 
 popd
+
+pushd /mnt/KingGhidora/media/shared/soundcloud_likes 
+date >> log
+/usr/local/bin/youtube-dl --add-metadata --no-progress -i --download-archive archive_list.txt --audio-quality 0 https://soundcloud.com/anthezium/likes 2 >&1 >> log 
+popd
